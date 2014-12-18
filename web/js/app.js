41b03499abca17faf4671e2931956b1cedf7e55e
Padmaja Peri <paddu.peri@gmail.com>
2014-12-17 21:21:29 -0800

    Format Code and Remove unwanted variables

--- web/js/app.js
+++ web/js/app.js
@@ -1,65 +1,65 @@
 //Declare Variables
 
 var apiKey,
-	sessionId,
-	token,
-	response,
-	session,
-	publisher;
+      sessionId,
+      token,
+      session,
+      publisher;
 
 
 //get the APIKEY and TOKEN 
-$(document).ready(function(){
+$(document).ready(function() {
 
-			getApiAndToken();
-			
-		});
+      getApiAndToken();
+
+});
 
 
-function getApiAndToken()
-{
-	$.get("/session",function(res){
+function getApiAndToken() {
+      $.get("/session", function(res) {
 
-			apiKey = res.apiKey;
+            apiKey = res.apiKey;
             sessionId = res.sessionId;
             token = res.token;
 
-            initializeSession(); 
+            initializeSession();
 
-	});
+      });
 
 }
 
 
-                
-function initializeSession()
-{
-			//Initialize Session Object
-            session = OT.initSession(apiKey, sessionId);
+
+function initializeSession() {
+      //Initialize Session Object
+      session = OT.initSession(apiKey, sessionId);
 
 
-            //Subscribe to a stream created
-            session.on("streamCreated",function(event){
-        			$("#subscriber").append("<div id='subscriber_div'></div>");                              	       
-					session.subscribe(event.stream,"subscriber_div",{width:"100%",height:"100%"});
-            });  
+      //Subscribe to a stream created
+      session.on("streamCreated", function(event) {
+            $("#subscriber").append("<div id='subscriber_div'></div>");
+            session.subscribe(event.stream, "subscriber_div", {
+                  width: "100%",
+                  height: "100%"
+            });
+      });
 
 
-            //Connect to the Session
-            session.connect(token, function(error)
-            {
+      //Connect to the Session
+      session.connect(token, function(error) {
 
-            		//If the connection is successful, initialize a publisher and publish to the session
-            		if(!error)
-            		{
-            			$("#publisher").append("<div id='publisher_div'></div>");
-                        publisher = OT.initPublisher("publisher_div",{width:"100%",height:"100%"});
+            //If the connection is successful, initialize a publisher and publish to the session
+            if (!error) {
+                  $("#publisher").append("<div id='publisher_div'></div>");
+                  publisher = OT.initPublisher("publisher_div", {
+                        width: "100%",
+                        height: "100%"
+                  });
 
-                        session.publish(publisher);
-                        
-            		}
+                  session.publish(publisher);
 
-            });                  
+            }
 
-}                  		
+      });
 
+}
