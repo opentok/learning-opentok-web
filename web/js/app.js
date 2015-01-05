f8cc5674678da584ced27a3319cf70099837b968
Padmaja Peri <paddu.peri@gmail.com>
2015-01-05 06:41:36 -0800

    Incorporated Review Comments

--- web/js/app.js
+++ web/js/app.js
@@ -1,65 +1,72 @@
 //Declare Variables
-
 var apiKey,
-      sessionId,
-      token,
-      session,
-      publisher;
+    sessionId,
+    token;
 
 
 //get the APIKEY and TOKEN 
+
 $(document).ready(function() {
 
-      getApiAndToken();
+    getApiAndToken();
 
 });
 
 
 function getApiAndToken() {
-      $.get("/session", function(res) {
+    $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res) {
 
-            apiKey = res.apiKey;
-            sessionId = res.sessionId;
-            token = res.token;
+        apiKey = res.apiKey;
+        sessionId = res.sessionId;
+        token = res.token;
 
-            initializeSession();
+        initializeSession();
 
-      });
+    });
 
 }
 
 
 
 function initializeSession() {
-      //Initialize Session Object
-      session = OT.initSession(apiKey, sessionId);
+    //Initialize Session Object
+    var session = OT.initSession(apiKey, sessionId);
 
 
-      //Subscribe to a stream created
-      session.on("streamCreated", function(event) {
-            $("#subscriber").append("<div id='subscriber_div'></div>");
-            session.subscribe(event.stream, "subscriber_div", {
-                  width: "100%",
-                  height: "100%"
+    //Subscribe to a newly created stream
+    session.on('streamCreated', function(event) {
+        session.subscribe(event.stream, 'subscriber', {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        });
+    });
+
+
+    //Handler for sessionDisconnected event
+    session.on('sessionDisconnected', function(event) {
+        console.log("The session got disconnected", event.reason);
+    });
+
+
+    //Connect to the Session
+    session.connect(token, function(error) {
+
+        //If the connection is successful, initialize a publisher and publish to the session
+        if (!error) {
+            var publisher = OT.initPublisher('publisher', {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
             });
-      });
 
+            session.publish(publisher);
 
-      //Connect to the Session
-      session.connect(token, function(error) {
+        } else {
 
-            //If the connection is successful, initialize a publisher and publish to the session
-            if (!error) {
-                  $("#publisher").append("<div id='publisher_div'></div>");
-                  publisher = OT.initPublisher("publisher_div", {
-                        width: "100%",
-                        height: "100%"
-                  });
+            console.log("There was an error connecting to the session", error.code, error.message);
+        }
 
-                  session.publish(publisher);
-
-            }
-
-      });
+    });
 
 }
