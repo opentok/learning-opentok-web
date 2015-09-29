6ab67fba7cf8a5dafac6fd9e85831002be09f592
Jeff Swartz <swartz@tokbox.com>
2015-09-29 16:16:10 -0700

    Archiving code.

--- web/js/app.js
+++ web/js/app.js
@@ -1,8 +1,13 @@
 var apiKey,
     sessionId,
-    token;
+    token,
+    status,
+    archiveID;
 
 $(document).ready(function() {
+  $('#stop').hide();
+  archiveID = null;
+
   // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
   $.get(SAMPLE_SERVER_BASE_URL + '/session', function(res) {
     apiKey = res.apiKey;
@@ -25,6 +30,21 @@ function initializeSession() {
     });
   });
 
+  session.on('archiveStarted', function(event) {
+    archiveID = event.id;
+    console.log('Archive started ' + archiveID);
+    $('#stop').show();
+    $('#start').hide();
+  });
+
+  session.on('archiveStopped', function(event) {
+    archiveID = event.id;
+    console.log('Archive stopped ' + archiveID);
+    $('#start').hide();
+    $('#stop').hide();
+    $('#view').show();
+  });
+
   session.on('sessionDisconnected', function(event) {
     console.log('You were disconnected from the session.', event.reason);
   });
@@ -45,3 +65,28 @@ function initializeSession() {
     }
   });
 }
+
+// Start recording
+function startArchive() {
+  $.post(SAMPLE_SERVER_BASE_URL + '/start/' + sessionId);
+  $('#start').hide();
+  $('#stop').show();
+}
+
+// Stop recording
+function stopArchive() {
+  $.post(SAMPLE_SERVER_BASE_URL + '/stop/' + archiveID);
+  $('#stop').hide();
+  $('#view').prop('disabled', false);
+  $('#stop').show();
+}
+
+// Get the archive status. If it is  "available", download it. Otherwise, keep checking
+// every 5 secs until it is "available"
+function viewArchive() {
+  $('#view').prop('disabled', true);
+  window.location = SAMPLE_SERVER_BASE_URL + '/view/' + archiveID;
+}
+
+$('#start').show();
+$('#view').hide();
